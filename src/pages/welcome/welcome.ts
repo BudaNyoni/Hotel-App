218502d95fd3fa9157e807125187761f519df135
BudaNyoni <nyonisbuda@gmail.com>
2018-08-30 14:41:32 +0200

    uyes

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -21,13 +21,13 @@ export class WelcomePage {
   Arr = [
     new Hotel('Montego bay Hotel','Relax and enjoy the beautiful white sandy beaches of Montego Bay, Jamaica. Plan your trip by discovering one-of-a-kind things to do, fantastic hotels, and more.','2000 for 4 Nights',
     "../../assets/ee9ee05e4471beac86a1d07d545419de.jpg","../../assets/8e529976583ca4e173b4154380418c59.jpg"),
-    new Hotel('African Safari','Imagine discovering luxury beachfront accommodation in Port Elizabeth that is as friendly and welcoming as the city that it calls home','1850 for 3 nights',
+    new Hotel('African Safari','The home of South Africas Big 5, with Luxury Safari Lodges to Suit every Need! Big 5 Safari. In Between Durban & JHB. Close To Drakensburg. Malaria Free.','1850 for 3 nights',
     "../../assets/98bd6c9bbcddd1b6d3050b3b5a7f52bf.jpg","../../assets/Top30PlacesInSouthAfrica_016.jpg"),
-    new Hotel('Table Bay Hotel','Imagine discovering luxury beachfront accommodation in Port Elizabeth that is as friendly and welcoming as the city that it calls home','3470 for 5 nights',
+    new Hotel('Table Bay Hotel','The Table Bay offers 5-star luxury hotel accommodation at V&A Waterfront in Cape Town with the views to match. Book conveniently online now.','3470 for 5 nights',
     "../../assets/eceadacfdabeeacfb03fa5d51294784d.jpg","../../assets/5720dbbb53f25ea6098122b77f7b43c1.jpg"),
-    new Hotel('The Palace of the Lost City Hotel','Imagine discovering luxury beachfront accommodation in Port Elizabeth that is as friendly and welcoming as the city that it calls home','2345 for 5 nights',
+    new Hotel('The Palace of the Lost City Hotel','Embark on an African soul safari. As part of Sun Internationals exclusive SunLux Collection, The Palace of the Lost City invites you to escape into the thrill of your journey. ','2345 for 5 nights',
     "../../assets/dfaacab1f4e22e5ff8f30ba6b520d7c5.jpg", "../../assets/610bd682955ccff530a644b9755260c3.jpg"),
-    new Hotel('Cape Grace Hotel','Imagine discovering luxury beachfront accommodation in Port Elizabeth that is as friendly and welcoming as the city that it calls home','4000 for 6 nights',
+    new Hotel('Cape Grace Hotel','Enjoy your stay and discover all we have to offer at Cape Grace in Cape Town, South Africa from The Leading Hotels of the World.','4000 for 6 nights',
     "../../assets/af6c09ead5a7d3789bc1f1ed29903122.jpg","../../assets/e750e1004fd32744f0363735026e04de.jpg")
   ];
   constructor(public navCtrl: NavController, public navParams: NavParams) {
